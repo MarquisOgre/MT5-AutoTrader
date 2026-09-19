c3cc2b2567a19f5a0d904224f2df912045286bbf
Marquis Ogre <marquisogre@gmail.com>
2026-09-19 19:19:15 +0530

    V8.8: add launcher

---
+++ V8.8/run_v8_8_fast_session_aware_24x7_m5.bat
@@ -0,0 +1,12 @@
+@echo off
+if /I not "%~1"== "MAX" (
+  start "DEXORZO Innovations - MT5 AutoTrader V8.8" /MAX cmd /k ""%~f0" MAX"
+  exit /b
+)
+cd /d "%~dp0"
+chcp 65001 >nul
+set PYTHONIOENCODING=utf-8
+title DEXORZO Innovations - MT5 AutoTrader V8.8 - FAST 24x7 M5
+python --version
+python v8_8_execution.py
+pause
